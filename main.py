f228c12d2e86f3f37747bae60638dac095f9e416
happySquid <rafael.safin077@gmail.com>
2024-07-02 17:47:38 -0400

    squid commit: added turn tracking, win or tie check, static buttons

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ from tkinter import ttk
 #Don't allow a button to be pressed again if it's been pressed before (without switching turn)
 #Connect AI and go brrrr
 
+# latest happysquid commit:
+# TODO: make it loop instead of quitting as it does now (erase squares and start over)
+# TODO: imo it should wait for input to reset squares to show the board when tied or won
+# TODO: or just create a reset button with condition text somewhere :D
+# added a turn switch (to determine current turn)
+# modified which letter is displayed/ added not allowing to switch already pressed button
+# modified the check for a win condition and added a check for a tie (tie check could be improved. i cant think of a better way rn lmao)
+
 #Setting up root window
 root = Tk()
 mainframe = ttk.Frame(root, borderwidth=3)
 mainframe.grid(column=0, row=0)
+
+# a switch to determine whose turn it is
+x_turn = True
 
 
 #Custom button class that stores its location for switching button text
@@ -21,31 +32,65 @@ class GameButton(ttk.Button):
 
     #Changes text of button to X or O, then calls check_win() to see if the move was a winning move
     def change_text(self):
+        global x_turn
         row = self.grid_info()['row']
         column = self.grid_info()['column']
 
-        if game_board[row][column].get() == '':
+        if game_board[row][column].get() != '':
+            return print('space taken')
+
+        if x_turn:
             game_board[row][column].set('X')
-        elif game_board[row][column].get() == 'X':
+            x_turn = False
+        else:
             game_board[row][column].set('O')
-        elif game_board[row][column].get() == 'O':
-            game_board[row][column].set('X')
+            x_turn = True
 
         print(f"{row} {column}")
         for i in range(len(game_board)):
             for j in range(len(game_board[i])):
                 print(game_board[i][j].get(), end=' ')
             print()
-        print("\n\n")
+        print("\n")
 
-        self.check_win()
+        self.check_tie_win()
 
-    def check_win(self):
-        if game_board[0][0].get() == 'X' and game_board[0][1].get() == 'X' and game_board[0][2].get() == 'X':
-            print("WIN!")
+    def check_tie_win(self):
+        x_and_o = ['X', 'O']
+        matrix = [  # horizontal
+                  [[0, 0], [0, 1], [0, 2]],
+                  [[1, 0], [1, 1], [1, 2]],
+                  [[2, 0], [2, 1], [2, 2]],
+                  # vertical
+                  [[2, 0], [1, 0], [0, 0]],
+                  [[2, 1], [1, 1], [0, 1]],
+                  [[2, 2], [1, 2], [0, 2]],
+                  # diagonal
+                  [[2, 0], [1, 1], [0, 2]],
+                  [[0, 0], [1, 1], [2, 2]]]
+        for e in x_and_o:
+            for row_of_three in matrix:
+                cords_1 = row_of_three[0]
+                cords_2 = row_of_three[1]
+                cords_3 = row_of_three[2]
+                if game_board[cords_1[0]][cords_1[1]].get() == e and game_board[cords_2[0]][cords_2[1]].get() == e and game_board[cords_3[0]][cords_3[1]].get() == e:
+                    # will quit on win change this so it stops and resets
+                    print("WIN!")
+                    root.quit()
+
+        empty = 0
+        for row in range(3):
+            for btn in range(3):
+                if game_board[row][btn].get() == '':
+                    empty += 1
+
+        if empty == 0:
+            # will quit on tie
+            print('Tie')
+            root.quit()
 
 
-global game_board
+
 game_board = [[StringVar() for x in range(3)] for y in range(3)]
 # Makes a 3x3 board of buttons and places them into the mainframe
 for i in range(len(game_board)):
